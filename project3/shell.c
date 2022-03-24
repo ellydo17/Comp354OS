f459557300fd35f5f83f673f6b9aea05bf19afca
tamrakarsweta220 <tamrakar0220@gmail.com>
2022-03-23 22:05:47 -0400

    fixed shell.c

--- project3/shell.c
+++ project3/shell.c
@@ -4,6 +4,7 @@ void main() {
   while(1){
     interrupt(0x21, 0, "Shell>\r\n\0", 0, 0);
     interrupt(0x21, 0x01, line, 0, 0);
+    interrupt(0x21, 0, "\n\0", 0, 0);
     interrupt(0x21, 0, "Unrecognized command\r\n\0", 0, 0);
   }
 }
