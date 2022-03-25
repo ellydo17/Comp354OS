3da178b318b919e5c44ef3efe80ca729bca7b795
tamrakarsweta220 <tamrakar0220@gmail.com>
2022-03-25 18:30:25 -0400

    compiled and linked userlib

--- project3/compileOS.bash
+++ project3/compileOS.bash
@@ -65,5 +65,16 @@ ld86 -o shell -d shell.o lib.o
 #loadFile is used to copy the contents of the shell file to the disk image
 ./loadfile shell
 
+#Improvements - creating a user library
+
+#compile shell.c with bcc
+bcc -ansi -c -o shell.o shell.c
+
+#to link the shell.o and lib.o files into the executable kernel file
+ld86 -o shell -d shell.o lib.o
+
+#loadFile is used to copy the contents of the shell file to the disk image
+./loadfile shell
+
 #run bochs to allow the bootloader to load the kernel
 bochs -f opsys.bxrc
