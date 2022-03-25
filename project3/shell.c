a812620224d3a56d3318b6978d8cb55aaf67670e
tamrakarsweta220 <tamrakar0220@gmail.com>
2022-03-25 19:11:48 -0400

    made improvements onn shell.c file

--- project3/shell.c
+++ project3/shell.c
@@ -5,158 +5,45 @@ char* getfilename(char* line);
 int compareCommand(char* cmd1, char* cmd2);
 
 void main() {
-  char line[80];
-  char* filename = "";
-  char* command = "";
-  int filenameIndex = 0;
-  
-  char* execute = "execute\0";
-  char* type = "type\0";
-  
-  char buffer[13312];
-  int i; //for resetting the buffer
-  char ch[1];
+  char command[80];
+  char buffer[13312]; //buffer stores the contents from the file when file is read/program is executed
+  int flag = 0; //true if file/program is found and can be executed
   
   while(1){
-    /*
-    interrupt(0x21, 0, "Shell> \0", 0, 0);
-    interrupt(0x21, 0x01, line, 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-    */
-    
-    printString("Shell>\0");
-    interrupt(0x21, 0x01, line, 0, 0);
-    interrupt(0x21, 0, "Unrecognized command\r\n\0", 0, 0);
-
-    /*
-    interrupt(0x21, 0, "line is: \0", 0, 0);
-    interrupt(0x21, 0, line, 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-    */
+    //Print out shell command as "Shell> "
+    printString("Shell> \0");
 
     //get the command
-    /*
-    command = getcommand(line);
+    readString(command);
+    printString("\r\n\0");
 
-    interrupt(0x21, 0, "command is:\0", 0, 0);
-    interrupt(0x21, 0, command, 0, 0);
-    interrupt(0x21, 0, "hmm\0", 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-
-    interrupt(0x21, 0, "line after getting the command name is: \0", 0, 0);
-    interrupt(0x21, 0, line, 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
+    //compare command and execute it accordingly
     
-    //get the file name
-    filename = getfilename(line);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-
-    
-    interrupt(0x21, 0, "filename is:\0", 0, 0);
-    interrupt(0x21, 0, filename, 0, 0);
-    interrupt(0x21, 0, "hmm\0", 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-
-    interrupt(0x21, 0, "line after getting the file name is: \0", 0, 0);
-    interrupt(0x21, 0, line, 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-
-    interrupt(0x21, 0, "command after getting the file name is:\0", 0, 0);
-    interrupt(0x21, 0, command, 0, 0);
-    interrupt(0x21, 0, "hmm\0", 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-    
-
-    if (compareCommand(command, execute) == 0){
-      interrupt(0x21, 0, "command is execute\r\n\0", 0, 0);
+    //command is "type"
+    if (command[0] == 't' && command[1] == 'y' && command[2] == 'p' && command[3] == 'e') {
+      printString("command is type\r\n\0");
       
-      //interrupt to execute file
-      interrupt(0x21, 0x04, filename, 0x2000, 0);
-      
-    } else if(compareCommand(command, type) == 0){
-      interrupt(0x21, 0, "command is type\r\n\0", 0, 0);
-      
-      //interrupt to read file
-      interrupt(0x21, 0x03, filename, buffer, 0);
-      //print out the file
-      interrupt(0x21, 0, buffer, 0, 0);
-      //reset the buffer
-      for (i=0; i <= 13312; i++) {
-	buffer[i] = 0x00;
+      flag = readFile(command+5, buffer);
+
+      if (flag != -1) { //file was found
+	printString(buffer);
+      } else { //file not found, display error
+	printString("Error: file not found\r\n\0");
       }
-      
-    } else {
-      interrupt(0x21, 0, "Unrecognized command\r\n\0", 0, 0);
+    } else if (command[0] == 'e' && command[1] == 'x'&& command[2] == 'e' && command[3] == 'c' && command[4] == 'u' && command[5] == 't' && command[6] == 'e') { //command is execute
+      printString("command is execute\r\n\0");
+
+      flag = executeProgram(command+8, 0x2000);
+
+      if (result == -1) { //file was found
+	printString("Error: cannot execute file\r\n\0");
+      } else if (result == -2) {
+	printString("Invalid segment\r\n\0");
+      } else {
+	printString("Program was executed\r\n\0");
+      }
+    } else { //invalid command
+      printString("Unrecognized command.\r\n\0");
     }
-    */
-  }
-}
-
-char* getcommand(char* line) {
-  char* command = "";
-  int i;
-  
-  while (line[i] != ' ') { //try to get the command name before the space
-    command[i] = line[i];
-    i++;
-  }
-  command[i] = '\0';
-  i++;
-  return command;
-}
-
-char* getfilename(char* line) {
-  int i=0;
-  int j=0;
-  char* filename = "";
-
-  while (line[i] != ' ') { //try to get the command name before the space
-    i++;
-  }
-  i++; //skip over the space character
-  
-  while (line[i] != '\0') { //read the file name from the characters after the space
-    filename[j] = line[i];
-    j++;
-    i++;
-  }
-  
-  filename[j] = '\0';
-  return filename;
-}
-
-int compareCommand(char* cmd1, char* cmd2) {
-  int flag=0; //default true, command is same
-
-  interrupt(0x21, 0, "compareCommand is called.\r\n\0", 0, 0);
-  
-  interrupt(0x21, 0, "cmd1 is\0", 0, 0);
-  interrupt(0x21, 0, cmd1, 0, 0);
-  interrupt(0x21, 0, "\r\n\0", 0, 0);
-
-  interrupt(0x21, 0, "cmd2 is\0", 0, 0);
-  interrupt(0x21, 0, cmd2, 0, 0);
-  interrupt(0x21, 0, "\r\n\0", 0, 0);
-
-  while(*cmd1 != '\0' && *cmd2 != '\0'){
-    interrupt(0x21, 0, "cmd1 is:\0", 0, 0);
-    interrupt(0x21, 0, *cmd1, 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-
-    interrupt(0x21, 0, "cmd2 is:\0", 0, 0);
-    interrupt(0x21, 0, *cmd2, 0, 0);
-    interrupt(0x21, 0, "\r\n\0", 0, 0);
-    
-    if(*cmd1 != *cmd2){ //commands not same
-      flag = 1; //false, command not same
-      break;
-    }
-    *cmd1++;
-    *cmd2++;
-  }
-  if(flag == 0){
-    return 0; //same
-  }else{
-    return 1; //not same
   }
 }
