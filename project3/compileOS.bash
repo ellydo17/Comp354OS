5e1f48342474eb1c893ec9853b78bf6e3afadea0
tamrakarsweta220 <tamrakar0220@gmail.com>
2022-03-25 18:46:38 -0400

    edited order in compileOS.basj

--- project3/compileOS.bash
+++ project3/compileOS.bash
@@ -34,14 +34,6 @@ gcc -o loadFile loadFile.c
 #assemble the lib.asm file
 as86 lib.asm -o lib.o
 
-#Improvements - creating a user library
-
-#compile userlib.c with bcc, link the userlib.o and lib.o files into the executable kernel file
-#and use loadfile to copy the contents of the shell file to the disk image
-bcc -ansi -c -o userlib.o userlib.c
-ld86 -o userlib -d userlib.o lib.o
-./loadfile userlib
-
 #compile uprog1.c with bcc, link the uprog1.o and lib.o files into the executable kernel file,
 #and use loadFile to copy the contents of the uprog1 file to the disk image
 bcc -ansi -c -o uprog1.o uprog1.c
@@ -56,13 +48,17 @@ bcc -ansi -c -o uprog2.o uprog2.c
 ld86 -o uprog2 -d uprog2.o lib.o
 ./loadfile uprog2
 
-#Command line shell
+#Command line shell, Improvements - creating a user library
+
+#compile userlib.c with bcc
+bcc -ansi -c -o userlib.o userlib.c
 
 #compile shell.c with bcc, link the shell.o and lib.o files into the executable kernel file,
 #and use loadfile to copy the contents of the shell file to the disk image
 bcc -ansi -c -o shell.o shell.c
-ld86 -o shell -d shell.o lib.o
+ld86 -o shell -d shell.o userlib.o lib.o
 ./loadfile shell
+#./loadfile userlib
 
 #run bochs to allow the bootloader to load the kernel
 bochs -f opsys.bxrc
