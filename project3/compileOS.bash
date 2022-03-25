c707196d04262ebf46265c2c9fa244b578140d3b
tamrakarsweta220 <tamrakar0220@gmail.com>
2022-03-25 18:41:55 -0400

    edited order in compileOS.basj

--- project3/compileOS.bash
+++ project3/compileOS.bash
@@ -31,50 +31,38 @@ gcc -o loadFile loadFile.c
 
 #Loading and Executing a User Program
 
-#to assemble the lib.asm file
+#assemble the lib.asm file
 as86 lib.asm -o lib.o
 
-#compile uprog1.c with bcc
+#Improvements - creating a user library
+
+#compile userlib.c with bcc, link the userlib.o and lib.o files into the executable kernel file
+#and use loadfile to copy the contents of the shell file to the disk image
+bcc -ansi -c -o userlib.o userlib.c
+ld86 -o userlib -d userlib.o lib.o
+./loadfile userlib
+
+#compile uprog1.c with bcc, link the uprog1.o and lib.o files into the executable kernel file,
+#and use loadFile to copy the contents of the uprog1 file to the disk image
 bcc -ansi -c -o uprog1.o uprog1.c
-
-#to link the uprog1.o and lib.o files into the executable kernel file
 ld86 -o uprog1 -d uprog1.o lib.o
-
-#loadFile is used to copy the contents of the uprog1 file to the disk image
 ./loadfile uprog1
 
 #Terminating a User Program
 
-#compile uprog1.c with bcc
+#compile uprog2.c with bcc, link the uprog2.o and lib.o files into the executable kernel file,
+#and use loadFile to copy the contents of the uprog2 file to the disk image
 bcc -ansi -c -o uprog2.o uprog2.c
-
-#to link the uprog2.o and lib.o files into the executable kernel file
 ld86 -o uprog2 -d uprog2.o lib.o
-
-#loadFile is used to copy the contents of the uprog2 file to the disk image
 ./loadfile uprog2
 
 #Command line shell
 
-#compile shell.c with bcc
+#compile shell.c with bcc, link the shell.o and lib.o files into the executable kernel file,
+#and use loadfile to copy the contents of the shell file to the disk image
 bcc -ansi -c -o shell.o shell.c
-
-#to link the shell.o and lib.o files into the executable kernel file
 ld86 -o shell -d shell.o lib.o
-
-#loadFile is used to copy the contents of the shell file to the disk image
 ./loadfile shell
-
-#Improvements - creating a user library
-
-#compile userlib.c with bcc
-bcc -ansi -c -o userlib.o userlib.c
-
-#to link the userlib.o and lib.o files into the executable kernel file
-ld86 -o userlib -d userlib.o lib.o
-
-#loadFile is used to copy the contents of the shell file to the disk image
-./loadfile userlib
 
 #run bochs to allow the bootloader to load the kernel
 bochs -f opsys.bxrc
