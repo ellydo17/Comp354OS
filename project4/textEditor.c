b5564d214ce5c62698ad8f7de87e4c0028ada4a1
tamrakarsweta220 <tamrakar0220@gmail.com>
2022-04-17 16:09:18 -0400

    edited text editor to read lines instead of single characters

--- project4/textEditor.c
+++ project4/textEditor.c
@@ -3,7 +3,10 @@
 void main(){
   char* filename;
   char fileBuffer[13312];
+  char line[80];
+  int lineIndex = 0;
   int fileBufferCursor;
+  int numCharRead;
   char ch;
   int sectors = 1;
 
@@ -17,7 +20,7 @@ void main(){
   printString("\r\n\0");
 
   //prompt the user to type in content for the file
-  printString("Please enter the content you want to write to this file. To quit writing, please enter Ctrl-D and hit return (enter button). \r\n\0");
+  printString("Please enter the content you want to write to this file. To quit writing, please hit enter and move to the next line, then hit Ctrl-D and and enter. \r\n\0");
 
   fileBufferCursor = 0;
   printString("initial value of fileBufferCursor is: \0");
@@ -26,13 +29,24 @@ void main(){
 
   while (fileBufferCursor < 13312) {
     printString("reading char \0");
-    ch = readString(fileBuffer, 80);
-    if (ch == 'q'){
+    
+    numCharRead = readString(line, 80); //read the string typed in by the user line by line
+    
+    if (fileBuffer[0] == 0x04){
       printString("User hit Ctrl-D and return.\r\n\0");
       break;
     }
-    fileBuffer[fileBufferCursor] = ch;
+
+    while(fileBufferCursor < numCharRead){
+      fileBuffer[fileBufferCursor] = line[lineIndex];
+      fileBufferCursor++;
+      lineIndex++;
+    }
+
+    fileBuffer[fileBufferCursor] = ' '; //adding space after each line to show continuity in the text
     fileBufferCursor++;
+    lineIndex = 0; //reset lineIndex for a new line
+    
     printString("Current fileBuffer Cursor is: \0");
     printInt(fileBufferCursor);
     printString(".\r\n\0");
